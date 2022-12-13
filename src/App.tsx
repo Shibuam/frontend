921aaa64ed88b6890de66da3ae9cbd6fbe76340d
Shibu <shibu.a5859@gmail.com>
2022-12-14 04:30:45 +0530

    added base name

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navigation from "./Componets/Navigation";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation />
+      <Router basename={window.location.pathname || ""}>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Login.tsx
@@ -0,0 +1,15 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+const Login = (): any => {
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col md={6}>1</Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Login;
